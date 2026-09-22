a8b3859cd48b902f1fa452173c560278602bf529
souravtripathi25 <sourav.tripathi2002@gmail.com>
2026-09-22 16:32:00 +0530

    test

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Sourav Tripathi"
+    return "Hello from cicd"
 
 @app.route("/health")
 def health():
--- test_app.py
+++ test_app.py
@@ -4,7 +4,7 @@ def test_home():
     client = app.test_client()
     response = client.get("/")
     assert response.status_code == 200
-    assert response.data == b"Hello from Sourav Tripathi"
+    assert response.data == b"Hello from cicd"
 
 def test_health():
     client = app.test_client()
